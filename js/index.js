2887c637a0a5a5dd8a8713f7c92e24fe62196e0f
James Gaddum <jwgaddum@gmail.com>
2018-02-28 18:40:40 +1300

    add images to carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 $('#sb_carousel').on('slide.bs.carousel', function(event) {
-    $("#sb_count").text((event.to + 1) + '/3')
+    $("#sb_count").text((event.to + 1) + '/9')
     $('#sb_caption').text(event.relatedTarget.innerText)
 })
