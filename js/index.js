89e5ad00689be9e1b7c60ceaf0a2e4d8472d9e7f
James Gaddum <jwgaddum@gmail.com>
2018-03-11 17:17:38 +1300

    add swipe to carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 $('#sb_carousel').on('slide.bs.carousel', function(event) {
     $("#sb_count").text((event.to + 1) + '/9')
     $('#sb_caption').text(event.relatedTarget.innerText)
-})
+});
+
+$("#sb_carousel").on("swiperight", function() {
+    console.log("Swiping right");
+    $("#sb_carousel").carousel('prev');
+});
+
+$("#sb_carousel").on("swipeleft", function() {
+    console.log("Swiping left");
+    $("#sb_carousel").carousel('next');
+});
